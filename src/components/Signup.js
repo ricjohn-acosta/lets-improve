78a8ff3188dae31502426d74a0eec7e3190b055c
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-03-21 03:51:21 +1300

    refactored signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,23 +2,21 @@ import React from "react";
 import { signup } from "../store/actions/auth"
 import { connect } from "react-redux";
 
-class Signup extends React.Component {
-  // handles user registration
-  handleSignup = e => {
+
+const Signup = ({signUp}) => {
+
+  function handleSignup(e) {
     e.preventDefault();
     let userEmail = e.target[0].value;
     let password = e.target[1].value;
     console.log(userEmail);
     console.log(password);
 
-    // Create user
-    this.props.signIn(userEmail, password);
-  };
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSignup}>
+    signUp(userEmail, password);
+  }
+  return (
+    <>
+      <form onSubmit={handleSignup}>
           Email:
           <input
             type="email"
@@ -42,14 +40,58 @@ class Signup extends React.Component {
           <br /> */}
           <button type="submit">Submit</button>
         </form>
-      </>
-    );
-  }
-}
+    </>
+  );
+};
+
+// class Signup extends React.Component {
+//   // handles user registration
+//   handleSignup = e => {
+//     e.preventDefault();
+//     let userEmail = e.target[0].value;
+//     let password = e.target[1].value;
+//     console.log(userEmail);
+//     console.log(password);
+
+//     // Create user
+//     this.props.signIn(userEmail, password);
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <form onSubmit={this.handleSignup}>
+//           Email:
+//           <input
+//             type="email"
+//             name="email"
+//             onChange={e => console.log(e.target.value)}
+//           />
+//           <br />
+//           Password:
+//           <input
+//             type="password"
+//             name="password"
+//             onChange={e => console.log(e.target.value)}
+//           />
+//           <br />
+//           {/* Username:
+//           <input
+//             type="text"
+//             name="username"
+//             onChange={e => console.log(e.target.value)}
+//           />
+//           <br /> */}
+//           <button type="submit">Submit</button>
+//         </form>
+//       </>
+//     );
+//   }
+// }
 
 function mapDispatchToProps(dispatch) {
   return {
-    signIn: (email, password) => dispatch(signup(email, password))
+    signUp: (email, password) => dispatch(signup(email, password))
   };
 }
 
