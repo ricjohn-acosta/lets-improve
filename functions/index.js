7099350a9fc0bb7dd01f29ac5659d4eef33cd3e8
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-04-25 23:50:01 +1200

    create test function

--- functions/index.js
+++ functions/index.js
@@ -9,48 +9,54 @@ admin.initializeApp();
 //  response.send("Hello from Firebase!");
 // });
 
-exports.myCloudTimer = functions.database
-  .ref("/startTimerRequest/")
-  .onCreate((event) => {
-    return admin.database().ref("cloudTimer/timeInMs").once("value", (snap) => {
-      if (!snap.exists()) {
-        return Promise.reject(new Error("error"));
-      }
+// exports.myCloudTimer = functions.database
+//   .ref("/startTimerRequest/")
+//   .onCreate((event) => {
+//     return admin.database().ref("cloudTimer/timeInMs").once("value", (snap) => {
+//       if (!snap.exists()) {
+//         return Promise.reject(new Error("error"));
+//       }
 
-      let timeInSeconds = snap.val() / 1000;
-      console.log("Cloud Timer was Started: " + timeInSeconds);
+//       let timeInSeconds = snap.val() / 1000;
+//       console.log("Cloud Timer was Started: " + timeInSeconds);
 
-      return functionTimer(timeInSeconds, (elapsedTime) => {
-        admin.database().ref("cloudTimer/observableTime").set(elapsedTime);
-      })
-        .then((totalTime) => {
-          console.log("Timer of " + totalTime + " has finished.");
-          return null;
-        })
-        .then(() => new Promise((resolve) => setTimeout(resolve, 1000)))
-        .then(() => event.data.ref.remove())
-        .catch((error) => console.error(error));
-    });
+//       return functionTimer(timeInSeconds, (elapsedTime) => {
+//         admin.database().ref("cloudTimer/observableTime").set(elapsedTime);
+//       })
+//         .then((totalTime) => {
+//           console.log("Timer of " + totalTime + " has finished.");
+//           return null;
+//         })
+//         .then(() => new Promise((resolve) => setTimeout(resolve, 1000)))
+//         .then(() => event.data.ref.remove())
+//         .catch((error) => console.error(error));
+//     });
+//   });
+
+// function functionTimer(seconds, call) {
+//   return new Promise((resolve, reject) => {
+//     if (seconds > 300) {
+//       reject(new Error("error"));
+//       return;
+//     }
+//     let interval = setInterval(onInterval, 1000);
+//     let elapsedSeconds = 0;
+
+//     function onInterval() {
+//       if (elapsedSeconds >= seconds) {
+//         clearInterval(interval);
+//         call(0);
+//         resolve(elapsedSeconds);
+//         return;
+//       }
+//       call(seconds - elapsedSeconds);
+//       elapsedSeconds++;
+//     }
+//   });
+// }
+
+exports.testFunction = functions.database
+  .ref("/createData/")
+  .onCreate((snapshot, context) => {
+    return admin.database().ref("functions/works");
   });
-
-function functionTimer(seconds, call) {
-  return new Promise((resolve, reject) => {
-    if (seconds > 300) {
-      reject(new Error("error"));
-      return;
-    }
-    let interval = setInterval(onInterval, 1000);
-    let elapsedSeconds = 0;
-
-    function onInterval() {
-      if (elapsedSeconds >= seconds) {
-        clearInterval(interval);
-        call(0);
-        resolve(elapsedSeconds);
-        return;
-      }
-      call(seconds - elapsedSeconds);
-      elapsedSeconds++;
-    }
-  });
-}
