122bec91271c490f4f12540efb6b91907365ee76
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-04-25 18:02:04 +1200

    fix cannot load module error

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require("firebase-functions");
-const firebase = require("firebase/database");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
@@ -9,17 +8,6 @@ admin.initializeApp();
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD3crIc4YzqQs5Vzr2F5DN7TzkTx00hd4s",
-  authDomain: "lets-improve.firebaseapp.com",
-  databaseURL: "https://lets-improve.firebaseio.com",
-  projectId: "lets-improve",
-  storageBucket: "lets-improve.appspot.com",
-  messagingSenderId: "396538697524",
-  appId: "1:396538697524:web:5b5da73b4b730eb5837761",
-  measurementId: "G-JNDQPS9W79",
-};
 
 exports.myCloudTimer = functions.database
   .ref("/startTimerRequest/")
